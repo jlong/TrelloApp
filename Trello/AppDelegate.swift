5bdebae55c9fee6ad0cbe94a23d840e4c3f5f9a6
John W. Long <john@uservoice.com>
2014-12-09 13:07:08 -0500

    Initial Commit

---
+++ Trello/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Trello
+//
+//  Created by John W. Long on 12/9/14.
+//  Copyright (c) 2014 John W. Long. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Trello/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Trello
+//
+//  Created by John W. Long on 12/9/14.
+//  Copyright (c) 2014 John W. Long. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
